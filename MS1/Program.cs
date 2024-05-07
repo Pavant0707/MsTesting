1e91eae1c3ec7d73b98b31ceeed5b8023e7c881c
Pavant0707 <pavant0707@gmail.com>
2024-05-07 16:26:16 +0530

    commit

---
+++ MS1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MS1
+{
+    public class Array
+    {
+
+        
+        public  int Add()
+        {
+            int[] a = { 1, 2, 3, 4 };
+            int sum = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                sum = sum + a[i];
+            }return sum;
+        }
+        public bool Contains1(string str) {
+            bool con=str.Contains("teja");
+            return con;
+        }
+        public string upercase(string str1)
+        {
+            string str2=str1.ToUpper();
+            return str2;
+        }
+        public string substring(string str1, string str2)
+        {
+            return "hi " +str1+" "+ str2;
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ MS1Tests1/ProgramTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MS1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MS1.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        Array a=new Array();
+        [TestMethod()]
+        public void AddTest()
+        {
+            int result = a.Add();
+            int ex = 10;
+            Assert.AreEqual(ex, result);
+        }
+        [TestMethod()]
+        public void AddTest1()
+        {
+            int result = a.Add();
+            int ex = 1;
+            Assert.AreEqual(ex, result);
+        }
+
+        [TestMethod()]
+        public void AddTest2()
+        {
+            bool result = a.Contains1("teja");
+            //string ex = "true";
+            Boolean ex=true;
+            Assert.AreEqual(ex, result);
+        }
+        [TestMethod()]
+        public void AddTest3()
+        {
+            string result = a.upercase("aaa");
+            string exe = "AAA";
+            Assert.AreEqual(exe, result);
+        }
+        [TestMethod()]
+        public void AddTest4()
+        {
+            string result = a.substring("king", "virat");
+            String exe = "hi king virat";
+            Assert.AreEqual(exe, result);
+        }
+    }
+}
